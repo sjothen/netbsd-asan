7fab512d9601135e3c2bfb4a34ce6b6f78a6a2ce
Stephen Jothen <sjothen@gmail.com>
2012-05-30 14:36:12 +0200

    Add __sysctl interceptor

---
+++ libc/asan/interceptors/__sysctl.c
@@ -0,0 +1,24 @@
+#include <sys/sysctl.h>
+#include <errno.h>
+
+int __sysctl(const int *, u_int, void *,
+		size_t *, const void *, size_t);
+int _asan__sysctl(const int *, u_int, void *,
+		size_t *, const void *, size_t);
+
+int
+__sysctl(const int *name, u_int namelen, void *old,
+		size_t *oldlenp, const void *new, size_t newlen)
+{
+	int ret = _asan__sysctl(name, namelen, old, oldlenp,
+			new, newlen);
+
+	if(ret == 0 || (ret == -1 && errno == ENOMEM)) {
+		if(old != NULL)
+			ASAN_WRITE_RANGE(old, *oldlenp);
+
+		ASAN_WRITE_RANGE(oldlenp, sizeof(size_t));
+	}
+
+	return ret;
+}
