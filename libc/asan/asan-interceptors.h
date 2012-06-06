a7ef3afcff01fad7c74f4d169aa33fd57efe9d44
Stephen Jothen <sjothen@gmail.com>
2012-06-06 16:11:25 +0200

    Add memory validation macros from ASan

--- libc/asan/asan-interceptors.h
+++ libc/asan/asan-interceptors.h
@@ -1,6 +1,31 @@
 #ifndef _ASAN_INTERCEPTORS_
 #define _ASAN_INTERCEPTORS_
 
+// Macros taken from ASan code (asan_interceptors.cc)
+#define ACCESS_ADDRESS(address, isWrite)   do {         \
+  if (AddressIsPoisoned(address)) {                     \
+    GET_CURRENT_PC_BP_SP;                               \
+    __asan_report_error(pc, bp, sp, address, isWrite, /* access_size */ 1); \
+  } \
+} while (0)
+
+#define ACCESS_MEMORY_RANGE(offset, size, isWrite) do { \
+  if (size > 0) { \
+    uintptr_t ptr = (uintptr_t)(offset); \
+    ACCESS_ADDRESS(ptr, isWrite); \
+    ACCESS_ADDRESS(ptr + (size) - 1, isWrite); \
+  } \
+} while (0)
+
+#define ASAN_READ_RANGE(offset, size) do { \
+  ACCESS_MEMORY_RANGE(offset, size, false); \
+} while (0)
+
+#define ASAN_WRITE_RANGE(offset, size) do { \
+  ACCESS_MEMORY_RANGE(offset, size, true); \
+} while (0)
+
+// Function to touch all of the bytes in a  null-terminated string.
 void touch_mem(const char *p);
 
 #endif
