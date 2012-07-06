7734d914eba82b4f7601b374c1f848b59944f203
Steve Jothen <sjothen@gmail.com>
2012-07-06 16:24:43 -0700

    Get lfs_bmapv interceptor compiling

--- libc/asan/interceptors/lfs_bmapv.c
+++ libc/asan/interceptors/lfs_bmapv.c
@@ -1,17 +1,19 @@
 #include "interceptors.h"
+#include <ufs/ufs/dinode.h>
+#include <sys/mount.h>
 #include <sys/types.h>
 #include <ufs/lfs/lfs.h>
 
-int lfs_bmapv(fsid_t *, BLOCK_INFO *, int);
-int _asan_lfs_bmapv(fsid_t *, BLOCK_INFO *, int);
+int lfs_bmapv(fsid_t *, struct block_info *, int);
+int _asan_lfs_bmapv(fsid_t *, struct block_info *, int);
 
 int
-lfs_bmapv(fsid_t *fsidp, BLOCK_INFO *blkiov, int blkcnt)
+lfs_bmapv(fsid_t *fsidp, struct block_info *blkiov, int blkcnt)
 {
 	int ret = _asan_lfs_bmapv(fsidp, blkiov, blkcnt);
 
 	if(ret == 0)
-		ASAN_WRITE_RANGE(blkiov, blkcnt*sizeof(BLOCK_INFO));
+		ASAN_WRITE_RANGE(blkiov, blkcnt*sizeof(struct block_info));
 
 	return ret;
 }
