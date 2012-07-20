11d8c5176a8dea666d86018ae44f0d020a28db06
Steve Jothen <sjothen@gmail.com>
2012-07-19 19:01:01 -0700

    Fix duplicate warnings in asan_syscalls.cc

--- compiler-rt/lib/asan/asan_syscalls.cc
+++ compiler-rt/lib/asan/asan_syscalls.cc
@@ -1,8 +1,73 @@
+#define mknod _mknod
+#define pollts _pollts
+#define msgctl _msgctl
+#define clock_getres _clock_getres
+#define __sigtimedwait ___sigtimedwait
+#define mq_timedreceive _mq_timedreceive
+#define mq_timedsend _mq_timedsend
+#define wait4 _wait4
+#define getrusage _getrusage
+#define nanosleep _nanosleep
+#define pselect _pselect
+#define select _select
+#define clock_gettime _clock_gettime
+#define clock_settime _clock_settime
+#define settimeofday _settimeofday
+
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/statvfs.h>
+#include <dirent.h>
+#include <sys/mount.h>
+#include <sys/param.h>
+#include <sys/timex.h>
+#include <sys/uio.h>
+#include <sys/signal.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/sysctl.h>
+#include <errno.h>
+#include <sys/ucontext.h>
+#include <sys/ksem.h>
+#include <lwp.h>
+#include <sys/sched.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <mqueue.h>
+#include <sys/msg.h>
+#include <sys/poll.h>
+#include <sys/select.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/extattr.h>
+#include <sys/event.h>
+#include <ufs/ufs/dinode.h>
+#include <ufs/lfs/lfs.h>
+#include <sys/module.h>
+#include <sys/pmc.h>
+#include <ufs/ufs/quota.h>
+#include <sys/shm.h>
+#include <sys/swap.h>
+#include <sys/uuid.h>
+
+#undef mknod
+#undef pollts
+#undef msgctl
+#undef clock_getres
+#undef __sigtimedwait
+#undef mq_timedreceive
+#undef mq_timedsend
+#undef wait4
+#undef getrusage
+#undef nanosleep
+#undef pselect
+#undef select
+#undef clock_gettime
+#undef clock_settime
+#undef settimeofday
 
 int __adjtime(const struct timeval *delta, struct timeval *olddelta)
 	__RENAME(_asan___adjtime);
@@ -23,12 +88,8 @@ int __fstat50(int fd, struct stat *sb)
 int __getcwd(char *bufp, size_t length)
 	__RENAME(_asan___getcwd);
 
-#include <dirent.h>
-
 int __getdents30(int fd, char *buf, size_t count)
 	__RENAME(_asan___getdents30);
-
-#include <sys/mount.h>
 
 int __getfh30(const char *fname, void *fhp, size_t *fh_size)
 	__RENAME(_asan___getfh30);
@@ -39,13 +100,9 @@ int __getlogin(char *namebuf, size_t namelen)
 int __lstat50(const char *path, struct stat *sb)
 	__RENAME(_asan___lstat50);
 
-#include <sys/param.h>
-
 int __mount50(const char *type, const char *path, int flags,
 		void *data, size_t data_len)
 	__RENAME(_asan___mount50);
-
-#include <sys/timex.h>
 
 int __ntp_adjtime(struct timex *tp)
 	__RENAME(_asan___ntp_adjtime);
@@ -59,8 +116,6 @@ int __posix_chown(const char *path, uid_t owner, gid_t group)
 int __posix_lchown(const char *path, uid_t owner, gid_t group)
 	__RENAME(_asan___posix_lchown);
 
-#include <sys/uio.h>
-
 ssize_t __preadv(int d, const struct iovec *iov, int iovcnt, off_t offset)
 	__RENAME(_asan___preadv);
 
@@ -70,13 +125,9 @@ ssize_t __pwritev(int d, const struct iovec *iov, int iovcnt, off_t offset)
 int __setlogin(const char *namebuf)
 	__RENAME(_asan___setlogin);
 
-#include <sys/signal.h>
-
 int __sigaction_sigtramp(int signum, const struct sigaction *nsa,
 		struct sigaction *osa, const void *tramp, int vers)
 	__RENAME(_asan___sigaction_sigtramp);
-
-#include <signal.h>
 
 int __sigaltstack14(const struct sigaltstack *nss,
 		struct sigaltstack *oss)
@@ -85,17 +136,12 @@ int __sigaltstack14(const struct sigaltstack *nss,
 int __sigpending14(sigset_t *set)
 	__RENAME(_asan___sigpending14);
 
-#include <time.h>
-
 int __sigtimedwait(const sigset_t *set, siginfo_t *info,
 		struct timespec *timeout)
 	__RENAME(_asan____sigtimedwait50);
 
 int __stat50(const char *path, struct stat *ub)
 	__RENAME(_asan___stat50);
-
-#include <sys/sysctl.h>
-#include <errno.h>
 
 int __sysctl(const int *name, u_int namelen, void *old,
 		size_t *oldlenp, const void *newp, size_t newlen)
@@ -104,12 +150,8 @@ int __sysctl(const int *name, u_int namelen, void *old,
 int __truncate(const char *path, int pad, off_t length)
 	__RENAME(_asan___truncate);
 
-#include <sys/ucontext.h>
-
 int _getcontext(struct __ucontext *ucp)
 	__RENAME(_asan__getcontext);
-
-#include <sys/ksem.h>
 
 int _ksem_getvalue(intptr_t id, unsigned int *value)
 	__RENAME(_asan__ksem_getvalue);
@@ -123,8 +165,6 @@ int _ksem_open(const char *name, int oflag, mode_t mode,
 
 int _ksem_unlink(const char *name)
 	__RENAME(_asan__ksem_unlink);
-
-#include <lwp.h>
 
 int _lwp_create(const ucontext_t *context, unsigned long flags, lwpid_t *new_lwp)
 	__RENAME(_asan__lwp_create);
@@ -159,8 +199,6 @@ int __sigprocmask14(int how, const sigset_t *set,
 int __sigsuspend14(const sigset_t *sigmask)
 	__RENAME(_asan__sys___sigsuspend14);
 
-#include <sys/sched.h>
-
 int _sched_getaffinity(pid_t pid, lwpid_t lid,
 		size_t size, cpuset_t *cpuset)
 	__RENAME(_asan__sys__sched_getaffinity);
@@ -177,8 +215,6 @@ int _sched_setparam(pid_t pid, lwpid_t lid, int policy,
 		const struct sched_param *params)
 	__RENAME(_asan__sys__sched_setparam);
 
-#include <sys/socket.h>
-
 int accept(int s, struct sockaddr * __restrict addr,
 		socklen_t * __restrict addrlen)
 	__RENAME(_asan__sys_accept);
@@ -190,14 +226,9 @@ int connect(int s, const struct sockaddr *name,
 int execve(const char *path, char * const *argp, char * const *envp)
 	__RENAME(_asan__sys_execve);
 
-#include <fcntl.h>
-#include <stdarg.h>
-
 int
 fcntl(int fd, int cmd, ...)
 	__RENAME(_asan_fcntl);
-
-#include <mqueue.h>
 
 ssize_t mq_receive(mqd_t mqdes, char *msg_ptr, size_t msg_len,
 		unsigned *msg_prio)
@@ -216,8 +247,6 @@ ssize_t mq_timedsend(mqd_t mqdes, const char *msg_ptr, size_t msg_len,
 		unsigned msg_prio, const struct timespec *abs_timeout)
 	__RENAME(_asan__sys_mq_timedsend);
 
-#include <sys/msg.h>
-
 ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg)
 	__RENAME(_asan__sys_msgrcv);
 
@@ -230,8 +259,6 @@ int nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
 int open(const char *path, int flags, ...)
 	__RENAME(_asan_open);
 
-#include <sys/poll.h>
-
 int poll(struct pollfd *fds, nfds_t nfds, int timeout)
 	__RENAME(_asan__sys_poll);
 
@@ -242,8 +269,6 @@ int pollts(struct pollfd * __restrict fds, nfds_t nfds,
 
 ssize_t pread(int fd, void *buf, size_t nread, off_t offset)
 	__RENAME(_asan__sys_pread);
-
-#include <sys/select.h>
 
 int pselect(int nfds, fd_set * __restrict readfds, fd_set * __restrict writefds,
 		fd_set * __restrict exceptfds, const struct timespec * __restrict timeout,
@@ -267,9 +292,6 @@ ssize_t readv(int d, const struct iovec *iov, int iovcnt)
 int select(int nfds, fd_set * __restrict readfds, fd_set * __restrict writefds,
 		fd_set * __restrict exceptfds, struct timeval * __restrict timeout)
 	__RENAME(_asan__sys_select);
-
-#include <sys/wait.h>
-#include <sys/resource.h>
 
 pid_t wait4(pid_t wpid, int *status, int options,
 		struct rusage *rusage)
@@ -313,8 +335,6 @@ int clock_gettime(clockid_t clock_id, struct timespec *tp)
 
 int clock_settime(clockid_t clock_id, const struct timespec *tp)
 	__RENAME(_asan_clock_settime);
-
-#include <sys/extattr.h>
 
 int extattr_delete_fd(int fd, int attrnamespace,
 		const char *attrname)
@@ -404,8 +424,6 @@ int __gettimeofday50(struct timeval * __restrict tp, void * __restrict tzp)
 int getvfsstat(struct statvfs *buf, size_t bufsize, int flags)
 	__RENAME(_asan_getvfsstat);
 
-#include <sys/event.h>
-
 int __kevent50(int kq, const struct kevent *changelist, size_t nchanges,
 		struct kevent *eventlist, size_t nevents,
 		const struct timespec *timeout)
@@ -419,9 +437,6 @@ int lchmod(const char *path, mode_t mode)
 
 int lchown(const char *path, uid_t owner, gid_t group)
 	__RENAME(_asan_lchown);
-
-#include <ufs/ufs/dinode.h>
-#include <ufs/lfs/lfs.h>
 
 int lfs_bmapv(fsid_t *fsidp, struct block_info *blkiov, int blkcnt)
 	__RENAME(_asan_lfs_bmapv);
@@ -441,8 +456,6 @@ int mkfifo(const char *path, mode_t mode)
 int mknod(const char *path, mode_t mode, dev_t dev)
 	__RENAME(_asan_mknod);
 
-#include <sys/module.h>
-
 int modctl(int cmd, void *arg)
 	__RENAME(_asan_modctl);
 
@@ -452,15 +465,11 @@ int msgctl(int msgid, int cmd, struct msqid_ds *buf)
 long pathconf(const char *path, int name)
 	__RENAME(_asan_pathconf);
 
-#include <sys/pmc.h>
-
 int pmc_get_info(int ctr, int op, void *args)
 	__RENAME(_asan_pmc_get_info);
 
 int profil(char *samples, size_t size, u_long offset, u_int scale)
 	__RENAME(_asan_profil);
-
-#include <ufs/ufs/quota.h>
 
 int quotactl(const char *path, int cmd, int id, void *addr)
 	__RENAME(_asan_quotactl);
@@ -505,8 +514,6 @@ int setsockopt(int s, int level, int name, const void *val,
 int settimeofday(const struct timeval *tv, const void *tzp)
 	__RENAME(_asan_settimeofday);
 
-#include <sys/shm.h>
-
 int __shmctl50(int shmid, int cmd, struct shmid_ds *buf)
 	__RENAME(_asan___shmctl50);
 
@@ -515,8 +522,6 @@ int socketpair(int d, int type, int protocol, int *sv)
 
 int statvfs1(const char *path, struct statvfs *buf, int flags)
 	__RENAME(_asan_statvfs1);
-
-#include <sys/swap.h>
 
 int swapctl(int cmd, void *arg, int misc)
 	__RENAME(_asan_swapctl);
@@ -547,8 +552,6 @@ int unmount(const char *path, int flags)
 
 int __utimes50(const char *path, const struct timeval times[2])
 	__RENAME(_asan___utimes50);
-
-#include <sys/uuid.h>
 
 int uuidgen(struct uuid *store, int count)
 	__RENAME(_asan_uuidgen);
