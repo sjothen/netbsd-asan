ef5edec85ce6421effda52238f8b29a0ef5f5f89
Steve Jothen <sjothen@gmail.com>
2012-08-03 18:33:34 -0700

    Add prototype for _cpuset_size for _sys_sched syscalls

--- libc/asan/interceptors/_asan__sys__sched_setaffinity.c
+++ libc/asan/interceptors/_asan__sys__sched_setaffinity.c
@@ -1,6 +1,8 @@
 #include "interceptors.h"
 #include <sys/sched.h>
 
+size_t _cpuset_size(const cpuset_t *);
+
 int _sys__sched_setaffinity(pid_t pid, lwpid_t lid,
 		size_t size, const cpuset_t *cpuset);
 int _asan__sys__sched_setaffinity(pid_t pid, lwpid_t lid,
@@ -14,7 +16,7 @@ _sys__sched_setaffinity(pid_t pid, lwpid_t lid,
 			size, cpuset);
 
 	if(ret == 0)
-		ASAN_READ_RANGE(cpuset, sizeof(cpuset_t));
+		ASAN_READ_RANGE(cpuset, cpuset_size(cpuset));
 
 	return ret;
 }
